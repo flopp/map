ddeef17fe85f20db24dac7ae66d15b7fd00c393f
Florian Pigorsch <fpigorsch@gmail.com>
2019-08-20 21:03:53 +0200

    Fix non-ascii characters in marker names

--- js/icon_factory.js
+++ js/icon_factory.js
@@ -38,32 +38,38 @@ export class IconFactory {
     create_map_icon(text, color) {
         const encoder = document.createElement('span');
         encoder.textContent = text;
-        const encoded_text = encoder.innerHTML;
+        const domString = encoder.innerHTML;
+        // domString is UTF-16; we need to convert it to UTF-8; see https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#Solution_4_%E2%80%93_escaping_the_string_before_encoding_it
+        const encoded_text = encodeURIComponent(domString).replace(/%([0-9A-F]{2})/g,
+            function toSolidBytes(match, p1) {
+                return String.fromCharCode('0x' + p1);
+            }
+        );
 
         const
             w = Math.max(33.0, 16.0 + this.compute_text_width(text, this.font)),
             w2 = 0.5 * w,
             text_color = color.text_color().to_hash_string(),
-            svg = `<svg\n
-                       xmlns:svg="http://www.w3.org/2000/svg"\n
-                       xmlns="http://www.w3.org/2000/svg"\n
-                       width="${w}" height="37"\n
-                       viewBox="0 0 ${w} 37"\n
-                       version="1.1">\n
-                     <defs>\n
-                       <filter id="shadow" x="0" y="0" width="100%" height="100%">\n
-                         <feOffset result="offOut" in="SourceAlpha" dx="1" dy="1" />\n
-                         <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2" />\n
-                         <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />\n
-                       </filter>\n
-                     </defs>\n
-                       <path\n
-                         fill="${color.to_hash_string()}" stroke="#000000"\n
-                         d="M 4 4 L 4 26 L ${w2 - 4.0} 26 L ${w2} 33 L ${w2 + 4.0} 26 L ${w - 4.0} 26 L ${w - 4.0} 4 L 4 4 z"\n
-                         filter="url(#shadow)" />\n
-                       <text\n
-                         style="text-anchor:middle;font-family:Arial,Helvetica,sans-serif;font-style:normal;font-weight:normal;font-size:16px;line-height:100%;font-family:sans;letter-spacing:0px;word-spacing:0px;fill:${text_color};fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"\n
-                         x="${w2}" y="21">${encoded_text}</text>\n
+            svg = `<svg
+                       xmlns:svg="http://www.w3.org/2000/svg"
+                       xmlns="http://www.w3.org/2000/svg"
+                       width="${w}" height="37"
+                       viewBox="0 0 ${w} 37"
+                       version="1.1">
+                     <defs>
+                       <filter id="shadow" x="0" y="0" width="100%" height="100%">
+                         <feOffset result="offOut" in="SourceAlpha" dx="1" dy="1" />
+                         <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2" />
+                         <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+                       </filter>
+                     </defs>
+                       <path
+                         fill="${color.to_hash_string()}" stroke="#000000"
+                         d="M 4 4 L 4 26 L ${w2 - 4.0} 26 L ${w2} 33 L ${w2 + 4.0} 26 L ${w - 4.0} 26 L ${w - 4.0} 4 L 4 4 z"
+                         filter="url(#shadow)" />
+                       <text
+                         style="text-anchor:middle;font-family:Arial,Helvetica,sans-serif;font-style:normal;font-weight:normal;font-size:16px;line-height:100%;font-family:sans;letter-spacing:0px;word-spacing:0px;fill:${text_color};fill-opacity:1;stroke:none;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+                         x="${w2}" y="21">${encoded_text}</text>
                    </svg>`,
             url = `data:image/svg+xml;charset=UTF-8;base64,${window.btoa(svg)}`;
 
