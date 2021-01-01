19b6c9135a0cf28bddcaf51372b065e4302ec723
florian <fpigorsch@gmail.com>
2021-01-01 12:38:39 +0100

    MapMenu: avoid jquery

--- js/map_menu.js
+++ js/map_menu.js
@@ -1,24 +1,22 @@
-import $ from 'jquery';
-
 export class MapMenu {
     constructor(app) {
         const self = this;
         this.app = app;
-        this.menu = $('#map-contextmenu');
-        this.addmarker = $('#map-contextmenu-addmarker');
-        this.deletemarker = $('#map-contextmenu-deletemarker');
-        this.projection = $('#map-contextmenu-projection');
-        this.centermap = $('#map-contextmenu-centermap');
+        this.menu = document.querySelector('#map-contextmenu');
+        this.addmarker = document.querySelector('#map-contextmenu-addmarker');
+        this.deletemarker = document.querySelector('#map-contextmenu-deletemarker');
+        this.projection = document.querySelector('#map-contextmenu-projection');
+        this.centermap = document.querySelector('#map-contextmenu-centermap');
         this.marker = null;
         this.coordinates = null;
 
-        this.addmarker.click(() => {
+        this.addmarker.addEventListener('click', () => {
             self.hide();
             self.app.map_state.add_marker(self.coordinates);
             return false;
         });
 
-        this.deletemarker.click(() => {
+        this.deletemarker.addEventListener('click', () => {
             self.hide();
             if (self.marker) {
                 self.app.map_state.delete_marker(self.marker.get_id());
@@ -27,7 +25,7 @@ export class MapMenu {
             return false;
         });
 
-        this.projection.click(() => {
+        this.projection.addEventListener('click', () => {
             self.hide();
             if (self.marker) {
                 self.app.show_projection_dialog(self.marker);
@@ -35,7 +33,7 @@ export class MapMenu {
             return false;
         });
 
-        this.centermap.click(() => {
+        this.centermap.addEventListener('click', () => {
             self.hide();
             if (self.coordinates) {
                 self.app.map_state.set_center(self.coordinates);
@@ -49,14 +47,14 @@ export class MapMenu {
     }
 
     hide() {
-        this.menu.css({display: 'none'});
+        this.menu.style.display = 'none';
     }
 
     showMap(wrapper, x, y, coordinates) {
-        this.addmarker.show();
-        this.deletemarker.hide();
-        this.projection.hide();
-        this.centermap.show();
+        this.addmarker.style.display = 'block';
+        this.deletemarker.style.display = 'none';
+        this.projection.style.display = 'none';
+        this.centermap.style.display = 'block';
 
         this.marker = null;
         this.coordinates = coordinates;
@@ -65,10 +63,10 @@ export class MapMenu {
     }
 
     showMarker(wrapper, x, y, marker) {
-        this.addmarker.hide();
-        this.deletemarker.show();
-        this.projection.show();
-        this.centermap.show();
+        this.addmarker.style.display = 'none';
+        this.deletemarker.style.display = 'block';
+        this.projection.style.display = 'block';
+        this.centermap.style.display = 'block';
 
         this.marker = marker;
         this.coordinates = null;
@@ -77,20 +75,8 @@ export class MapMenu {
     }
 
     show(wrapper, x, y) {
-        this.hide();
-
-        let xx = x;
-        let yy = y;
-
-        // Adjust the menu if clicked to close to the edge of the map
-        if (x > wrapper.width() - this.menu.width()) {
-            xx = wrapper.width() - this.menu.width();
-        }
-
-        if (y > wrapper.height() - this.menu.height()) {
-            yy = wrapper.height() - this.menu.height();
-        }
-
-        this.menu.css({top: yy, left: xx, display: 'block'});
+        this.menu.style.top = `${Math.min(y, wrapper.height() - this.menu.clientHeight)}px`;
+        this.menu.style.left = `${Math.min(x, wrapper.width() - this.menu.clientWidth)}px`;
+        this.menu.style.display = 'block';
     }
 }
