d3d3fd87a6c96863bb88810f48aa2cf61a9955f6
Florian Pigorsch <fpigorsch@gmail.com>
2021-05-02 16:22:40 +0200

    simplify context menu

--- src/components/map_menu.ts
+++ src/components/map_menu.ts
@@ -2,6 +2,7 @@ import {App} from "./app.js";
 import {Coordinates} from "./coordinates.js";
 import {Marker} from "./marker";
 import {MapWrapper} from "./map_wrapper.js";
+import {create_element} from "./utilities";
 
 export class MapMenu {
     public app: App;
@@ -9,27 +10,28 @@ export class MapMenu {
     public addMarker: HTMLElement;
     public deleteMarker: HTMLElement;
     public projection: HTMLElement;
-    public centerMap: HTMLElement;
     public marker: Marker;
     public coordinates: Coordinates;
 
     constructor(app: App) {
         const self = this;
         this.app = app;
-        this.menu = document.querySelector('#map-contextmenu');
-        this.addMarker = document.querySelector('#map-contextmenu-add-marker');
-        this.deleteMarker = document.querySelector('#map-contextmenu-delete-marker');
-        this.projection = document.querySelector('#map-contextmenu-projection');
-        this.centerMap = document.querySelector('#map-contextmenu-center-map');
-        this.marker = null;
-        this.coordinates = null;
 
+        this.menu = create_element("div", ["dropdown-menu"], {"role": "menu"});
+        document.getElementsByTagName("body")[0].appendChild(this.menu);
+        const content = create_element("div", ["dropdown-content"]);
+        this.menu.appendChild(content);
+
+        this.addMarker = create_element("a", ["dropdown-item"], {"data-i18n": "menu.add-marker"});
+        content.appendChild(this.addMarker);
         this.addMarker.addEventListener('click', (): boolean => {
             self.hide();
             self.app.map_state.add_marker(self.coordinates);
             return false;
         });
 
+        this.deleteMarker = create_element("a", ["dropdown-item"], {"data-i18n": "menu.delete-marker"});
+        content.appendChild(this.deleteMarker);
         this.deleteMarker.addEventListener('click', (): boolean => {
             self.hide();
             if (self.marker) {
@@ -39,6 +41,8 @@ export class MapMenu {
             return false;
         });
 
+        this.projection = create_element("a", ["dropdown-item"], {"data-i18n": "menu.waypoint-projection"});
+        content.appendChild(this.projection);
         this.projection.addEventListener('click', (): boolean => {
             self.hide();
             if (self.marker) {
@@ -47,7 +51,9 @@ export class MapMenu {
             return false;
         });
 
-        this.centerMap.addEventListener('click', (): boolean => {
+        const centerMap = create_element("a", ["dropdown-item"], {"data-i18n": "menu.center-map"});
+        content.appendChild(centerMap);
+        centerMap.addEventListener('click', (): boolean => {
             self.hide();
             if (self.coordinates) {
                 self.app.map_state.set_center(self.coordinates);
@@ -56,6 +62,9 @@ export class MapMenu {
             }
             return false;
         });
+
+        this.marker = null;
+        this.coordinates = null;
 
         this.hide();
     }
@@ -68,7 +77,6 @@ export class MapMenu {
         this.addMarker.style.display = 'block';
         this.deleteMarker.style.display = 'none';
         this.projection.style.display = 'none';
-        this.centerMap.style.display = 'block';
 
         this.marker = null;
         this.coordinates = coordinates;
@@ -80,7 +88,6 @@ export class MapMenu {
         this.addMarker.style.display = 'none';
         this.deleteMarker.style.display = 'block';
         this.projection.style.display = 'block';
-        this.centerMap.style.display = 'block';
 
         this.marker = marker;
         this.coordinates = null;
