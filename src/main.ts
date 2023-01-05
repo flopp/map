1c7b9b6bf5921d2ac78953bf37b311042e1f6578
Florian Pigorsch <fpigorsch@gmail.com>
2023-01-05 18:40:40 +0100

    avoid flickering in start-up

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import {App} from "./components/app";
 import "./styles/styles.scss";
 
 const main = (): void => {
+    // remove "display:none" from body
+    document.body.style.display = "block";
     (window as any).app = new App("map-container-leaflet");
 };
 
+console.log(document.readyState);
 if (document.readyState === "loading") {
     console.log("add event listener");
     document.addEventListener("DOMContentLoaded", main);
